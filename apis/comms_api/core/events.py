de7491911c8fc800c84fb8937b13a549b7f24ea7
RamosFe <federico.ramos@wazuh.com>
2024-08-23 18:12:53 -0300

    Intagrated Batcher with comms_api.
    Deleted IndexerConfig.
    Added get_indexer_client() to the Batcher.
    Changed how events are created by using the Batcher instead of the
    Indexer.

--- apis/comms_api/core/events.py
+++ apis/comms_api/core/events.py
@@ -1,5 +1,7 @@
 from wazuh.core.indexer import get_indexer_client
 from wazuh.core.indexer.models.events import Events
+from wazuh.core.batcher.client import BatcherClient
+from comms_api.core.batcher import batcher_mux_demux_manager
 
 
 async def create_stateful_events(events: Events) -> dict:
@@ -16,4 +18,5 @@ async def create_stateful_events(events: Events) -> dict:
         Dictionary with the indexer response.
     """
     async with get_indexer_client() as indexer_client:
-        return await indexer_client.events.create(events)
+        batcher_client = BatcherClient(queue=batcher_mux_demux_manager.get_queue())
+        return await indexer_client.events.create(events, batcher_client)
