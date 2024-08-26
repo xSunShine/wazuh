22aeb2369a22779c2319df0299f295cac8c15d2e
RamosFe <federico.ramos@wazuh.com>
2024-08-26 09:03:21 -0300

    Fixed integration of batcher in comms_api.

--- apis/comms_api/routers/events.py
+++ apis/comms_api/routers/events.py
@@ -1,4 +1,4 @@
-from fastapi import status
+from fastapi import status, Request
 from fastapi.responses import JSONResponse
 
 from comms_api.core.events import create_stateful_events
@@ -9,7 +9,7 @@ from wazuh.core.indexer.models.events import Events
 
 
 @timeout(30)
-async def post_stateful_events(events: Events) -> JSONResponse:
+async def post_stateful_events(request: Request, events: Events) -> JSONResponse:
     """Post stateful events handler.
 
     Parameters
@@ -28,7 +28,7 @@ async def post_stateful_events(events: Events) -> JSONResponse:
         Indexer response.
     """
     try:
-        response = await create_stateful_events(events)
+        response = await create_stateful_events(events, request.app.state.batcher_queue)
         return JSONResponse(response)
     except WazuhError as exc:
         raise HTTPError(message=exc.message, status_code=status.HTTP_400_BAD_REQUEST)
